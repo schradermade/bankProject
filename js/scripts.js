950eac7a8e3ce84b4a86b9324f8eceb4a9db4b3a
Nathan Schrader <nathanschrader@icloud.com>
2020-11-07 19:48:21 -0800

    add withdrawal prototype and user input fields for deposit and withdrawal
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 //*********************************************************************//
     // Business Logic //
 //*********************************************************************//
-function Account(firstName, lastName, beginningBalance)  {
+function Account(firstName, lastName, beginningBalance) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.beginningBalance = beginningBalance;
+  this.currentBalance = 0;
+  // this.withdrawal;
+}
+
+Account.prototype.withdrawal = function() {
+  console.log(this.beginningBalance);
+  // this.currentBalance = this.currentBalance - this.withdrawal;
+  // return this.currentBalance;
+  
 }
 
 // Front End Logic
@@ -15,10 +24,9 @@ $(document).ready(function() {
     let lastName = $('#lastName').val();
     let beginningBalance = parseFloat($('#beginningBalance').val());
     let account1 = new Account(firstName, lastName, beginningBalance);
-    alert(account1.firstName);
-    alert(account1.lastName);
     console.log(account1);
-  
-
+    account1.withdrawal();
+    $(".userBeginningBalance").append("This is your beginning balance: " + beginningBalance)
+    $(".userCurrentBalance").append("This is your current balance: " + currentBalance)
   });
 });
